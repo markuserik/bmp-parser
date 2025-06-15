c9ac9996d335f36a0f2c08dcc54c6c63773b2f3b
markuserik <markus13671@gmail.com>
2025-06-15 18:58:44 +0200

    Read file

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 
 pub fn main() !void {
     try get_args();
@@ -8,6 +9,18 @@ pub fn main() !void {
         std.debug.print("Expected: {s} input.bmp\n", .{args[0]});
         return;
     }
+
+    const file = try fs.cwd().openFile(args[1], .{});
+    defer file.close();
+
+    const file_size = (try file.stat()).size;
+    const contents = try file.reader().readAllAlloc(allocator, file_size);
+    defer allocator.free(contents);
+    for (contents) |byte| {
+        std.debug.print("{x}", .{byte});
+    }
+
+    std.debug.print("\nLen: {}\n", .{contents.len});
 }
 
 var allocator: std.mem.Allocator = undefined;
