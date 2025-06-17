2d74df3cddca0713d745c1f65c3184e4ece86bf8
markuserik <markus13671@gmail.com>
2025-06-17 12:35:47 +0200

    Rename translate_hex to parse_hex

--- src/bmp.zig
+++ src/bmp.zig
@@ -23,13 +23,13 @@ pub fn parse(file_contents: []u8) !bmp {
 fn parse_file_header(file_header_raw: []u8) !bitmap_file_header {
     return bitmap_file_header{
         .identifier = file_header_raw[0..2].*,
-        .file_size = try translate_hex(file_header_raw[2..6]),
+        .file_size = try parse_hex(file_header_raw[2..6]),
         .reserved1 = file_header_raw[6..8].*,
         .reserved2 = file_header_raw[8..10].*,
-        .offset = try translate_hex(file_header_raw[10..14])
+        .offset = try parse_hex(file_header_raw[10..14])
     };
 }
 
-fn translate_hex(slice: []u8) !u32 {
+fn parse_hex(slice: []u8) !u32 {
     return @as(u32, slice[0]) | @as(u32, slice[1]) << 8 | @as(u32, slice[2]) << 16 | @as(u32, slice[3]) << 24;
 }
