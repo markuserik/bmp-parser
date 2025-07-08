a5743bbcf6f1975fb36bbc972295ef34698f1e9e
markuserik <markus13671@gmail.com>
2025-07-08 19:51:44 +0200

    Turn project into a library

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # bmp-parser
 
-A parser for .bmp files. Currently, it takes a file as an argument and prints parsed values from it.
-
-## Run
-
-```sh
-./bmp-parser image.bmp
-```
+A zig parsing library for .bmp files.
--- build.zig
+++ build.zig
@@ -5,36 +5,16 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const exe = b.addExecutable(.{
-        .name = "bmp_parser",
-        .root_module = exe_mod,
-    });
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const module = b.addModule("bmp_parser", .{ .root_source_file = b.path("src/bmp.zig")});
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const bmp_test = b.addTest(.{
+        .root_source_file = b.path("src/bmp.zig"),
+        .optimize = optimize,
+        .target = target
+    });
+    const run_test = b.addRunArtifact(bmp_test);
+    bmp_test.root_module.addImport("bmp_parser", module);
+    test_step.dependOn(&run_test.step);
+    b.default_step.dependOn(test_step);
 }
