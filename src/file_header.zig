d11e6652b47d219b85aa15cbcc5fde279723c305
markuserik <markus13671@gmail.com>
2025-10-17 09:34:43 +0200

    Discard identifier

--- src/file_header.zig
+++ src/file_header.zig
@@ -3,15 +3,16 @@ const fs = std.fs;
 
 const File_header = @This();
 
-identifier: [2]u8,
 file_size: u32,
 reserved1: [2]u8,
 reserved2: [2]u8,
 offset: u32,
 
 pub fn parseFileHeader(reader: *std.io.Reader, endianness: std.builtin.Endian) !File_header {
+    // Discard identifier
+    _ = try reader.take(2);
+
     return File_header{
-        .identifier = (try reader.takeArray(2)).*,
         .file_size = try reader.takeInt(u32, endianness),
         .reserved1 = (try reader.takeArray(2)).*,
         .reserved2 = (try reader.takeArray(2)).*,
